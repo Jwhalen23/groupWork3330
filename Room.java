a930cfa2953cc76d3230ecbe8c478f7f1448f6be
Jwhalen23 <120579598+Jwhalen23@users.noreply.github.com>
2026-02-18 16:49:11 -0600

    Add files via upload
    added these 2 classes

--- Room.java
+++ Room.java
@@ -1,9 +1,32 @@
 package library;
 
 public class Room {
+	private final String roomName; 
+	private final int capacity; 
 
-	public Room() {
-		// TODO Auto-generated constructor stub
+
+	public Room(String roomName, int capacity) {
+		if (roomName == null || roomName.trim().isEmpty()) {
+			throw new IllegalArgumentException("Room Must have a name!")
+			// Make Sure Room has a name. 
+		}
+		if (capacity <= 0) {
+			throw new IllegalArgumentException("Capacity must be positive!")
+			// Ensuring no negatives for capacity 
+		}
+		this.roomName = roomName;
+		this.capacity = capacity;
 	}
+	
+	public String getRoomname() { // getter
+		return roomName;
+	}
+	
+	@Override // Checks for spelling
+	public String toString() {
+		return roomName + "(capacity:" + capacity + ")";
+	}
+		
+		
 
 }
--- TimeSlot.java
+++ TimeSlot.java
@@ -1,9 +1,25 @@
 package library;
 
 public class TimeSlot {
+	private final int hour;
 
-	public TimeSlot() {
-		// TODO Auto-generated constructor stub
+	public TimeSlot(int hour) {
+		if (hour < 0 || hour > 23) {
+			throw new IllegalArgumentException("Hour must be between 0 and 24.") // make sure hours are between 0 and 24
+		}
+		this.hour = hour;
 	}
-
+	public int getHour() {  // getter 
+		return hour;
+	}
+	@0verride 
+	public String toString() {
+		int displayHour = (hour == 0 || hour == 12) ? 12 : hour % 12;
+		String dayTime = (hour < 12) ? "AM" : "PM";  // determine day or night 
+		int nextHour = (hour + 1) % 24;
+		int nextDisplayHour = (nextHour == 0 || nextHour == 12) ? 12 : nextHour % 12;
+		String nextTime = (nextHour < 12) ? "AM" : "PM";
+        return String.format("%d:00%s-%d:00%s", displayHour, dayTime, nextDisplayHour, nextTime);
+	}
 }
+	
